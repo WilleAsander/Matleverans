06ac80064c27debc83162e37f7d76865fb748a69
Testo12 <erik.malmsten99@gmail.com>
2018-10-31 14:52:57 +0100

    Added basics for button and info boxes

--- MatLeverans/Form1.cs
+++ MatLeverans/Form1.cs
@@ -12,9 +12,33 @@ namespace MatLeverans
 {
     public partial class Form1 : Form
     {
+        private ICustomer register;
+
         public Form1()
         {
             InitializeComponent();
+
+            register = new ICustomer();
+
+            button1.Text = ("Register!");
+
+            button1.Click += new EventHandler(DoRegister);
+
+        }
+
+        private void DoRegister(object sender, EventArgs e)
+        {
+
+
+            string name = (textBox1.Text);
+            string socialSec = (textBox2.Text);
+            string adress = (textBox3.Text);
+            string email = (textBox4.Text);
+            string phone = (textBox5.Text);
+            bool VIP = (checkBox1.Checked);
+
+
+
         }
     }
 }
