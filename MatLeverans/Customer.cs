5005b4184c79e53613da0e288247d83fd31e6ce6
RyuTribal <ivan.sedelkin9@gmail.com>
2018-10-31 14:43:08 +0100

    Added an interface and class only for add function

---
+++ MatLeverans/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatLeverans
+{
+    public class Customer : ICustomer
+    {
+        public List<Customer> Add(string name, string socialSec, string adress, string email, string phone, bool VIP, int id, string date)
+        {
+            return new List<Customer>();
+
+        }
+    }
+}
---
+++ MatLeverans/ICustomer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatLeverans
+{
+    public interface ICustomer
+    {
+        List<Customer> Add(string name, string socialSec, string adress, string email,string phone, bool VIP, int id,string date);
+    }
+}
